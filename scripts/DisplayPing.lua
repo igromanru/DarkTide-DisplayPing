a83d413c9ae8eaf0a6f940699ae55da02d89d552
igromanru <igromanru@yahoo.de>
2025-01-18 18:25:23 +0100

    chore: Bump version for release

--- scripts/DisplayPing.lua
+++ scripts/DisplayPing.lua
@@ -2,7 +2,7 @@
     Author: Igromanru
     Date: 16.12.2024
     Mod Name: Display Ping
-    Version: 1.6.0
+    Version: 1.6.1
 ]]
 local mod = get_mod("DisplayPing")
 mod:io_dofile("DisplayPing/scripts/DisplayPing_settings")
