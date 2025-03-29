4ecb750a6a45511a888d173ad5045f1054df8464
xsSplater - СанСаныч <xssplater@gmail.com>
2025-03-29 19:04:34 +0300

    Update DisplayPing_localization.lua

--- scripts/DisplayPing_localization.lua
+++ scripts/DisplayPing_localization.lua
@@ -10,193 +10,241 @@ local InputUtils = require("scripts/managers/input/input_utils")
 local localizations = {
   mod_name = {
     en = "Display Ping",
+    ru = "Индикатор пинга",
   },
   mod_description = {
     en = "Displays your current ping",
+    ru = "Display Ping - Отображает ваш текущий пинг",
   },
   [SettingNames.EnableMod] = {
     en = "Enable",
     ["zh-cn"] = "开启",
+    ru = "Включить",
   },
   [SettingNames.TacticalOverlayOnly] = {
     en = "Show only in tactical overlay",
     ["zh-cn"] = "仅在Tab页面显示",
+    ru = "Показывать только в тактическом оверлее",
   },
   [SettingNames.HideInLobby] = {
     en = "Hide while in Mourningstar",
     ["zh-cn"] = "处于大厅时隐藏延迟信息",
+    ru = "Скрывать в Моунингстар",
   },
   [SettingNames.HideInLobbyTooltip] = {
     en = "Shows Ping only in tactical overlay while in Mourningstar",
     ["zh-cn"] = "当处于大厅时，仅在Tab页面显示延迟信息",
+    ru = "Показывает пинг только в тактическом оверлее в Моунингстар",
   },
   [SettingNames.ShowAveragePing] = {
     en = "Show Average Ping",
     ["zh-cn"] = "显示平均延迟",
+    ru = "Показать средний пинг",
   },
   [SettingNames.ShowAveragePingTooltip] = {
     en = "Shows average ping in selected time frame. When disabled, the mod displays last measurement.",
     ["zh-cn"] = "显示选定时间范围内的平均延迟。禁用该选项后，仅显示当前的延迟",
+    ru = "Показывает средний пинг в выбранном временном интервале. При отключении мод отображает последнее измерение.",
   },
   [SettingNames.ShowAveragePingTimeFrame] = {
     en = "Average Ping time frame in seconds",
     ["zh-cn"] = "设定时间范围",
+    ru = "Среднее время пинга в секундах",
   },
   [SettingNames.PositionOnScreen] = {
     en = "Position on Screen",
     ["zh-cn"] = "设置屏幕上所处的位置",
+    ru = "Положение на экране",
   },
   [SettingNames.CustomHudMode] = {
     en = "Custom HUD Mode",
     ["zh-cn"] = "Custom HUD 模式",
+    ru = "Режим Custom HUD",
   },
   [SettingNames.PingHorizontalAlignment] = {
     en = "Horizontal Alignment",
     ["zh-cn"] = "水平对齐",
+    ru = "Горизонтальное выравнивание",
   },
   [SettingNames.PingVerticalAlignment] = {
     en = "Vertical Alignment",
     ["zh-cn"] = "垂直对齐",
+    ru = "Вертикальное выравнивание",
   },
   [SettingNames.CustomHudModeTooltip] = {
     en = "Prevents the mod from updating it's position on the screen, which allows other mods, like Custom HUD, to manage it.",
     ["zh-cn"] = "为防止因MOD更新导致屏幕上所在的位置发生改变，开启此选项可允许由其他MOD管理（例如Custom HUD）",
+    ru = "Не позволяет моду обновлять своё положение на экране, что позволяет другим модам, таким как Custom HUD, управлять им.",
   },
   [SettingNames.PingXOffset] = {
     en = "X Offset",
     ["zh-cn"] = "X轴偏移值",
+    ru = "Смещение по вертикали",
   },
   [SettingNames.PingYOffset] = {
     en = "Y Offset",
     ["zh-cn"] = "Y轴偏移值",
+    ru = "Смещение по горизонтали",
   },
   [SettingNames.PingStyleGroup] = {
     en = "Ping Style Settings",
     ["zh-cn"] = "设置延迟样式",
+    ru = "Настройки стиля пинга",
   },
   [SettingNames.PingFontSize] = {
     en = "Text Size",
     ["zh-cn"] = "文字大小",
+    ru = "Размер текста",
   },
   [SettingNames.PingLabel] = {
     en = "Label",
     ["zh-cn"] = "文字",
+    ru = "Название",
   },
   [SettingNames.PingDefaultColor] = {
     en = "Default Color",
     ["zh-cn"] = "默认颜色",
+    ru = "Цвет по умолчанию",
   },
   [SettingNames.PingRangeIndicatorGroup] = {
     en = "Dynamic Color Range",
     ["zh-cn"] = "动态颜色范围",
+    ru = "Динамический цветовой диапазон",
   },
   [SettingNames.PingRangeIndicator] = {
     en = "Enable Dynamic Color Range",
     ["zh-cn"] = "启用动态颜色范围",
+    ru = "Включить динамический цветовой диапазон",
   },
   [SettingNames.PingLowColor] = {
     en = "Low Ping Color",
     ["zh-cn"] = "低延迟颜色",
+    ru = "Цвет низкого пинга",
   },
   [SettingNames.PingMiddleColor] = {
     en = "Acceptable Ping Color",
     ["zh-cn"] = "中等延迟颜色",
+    ru = "Цвет приемлемого пинга",
   },
   [SettingNames.PingHighColor] = {
     en = "High Ping Color",
     ["zh-cn"] = "高延迟颜色",
+    ru = "Цвет высокого пинга",
   },
   [SettingNames.PingLowMinValue] = {
     en = "Low Ping Range Start",
     ["zh-cn"] = "低延迟初始值",
+    ru = "Начало диапазона низкого пинга",
   },
   [SettingNames.PingMiddleMinValue] = {
     en = "Acceptable Ping Range Start",
     ["zh-cn"] = "中等延迟初始值",
+    ru = "Начало диапазона приемлемого пинга",
   },
   [SettingNames.PingHighMinValue] = {
     en = "High Ping Range Start",
     ["zh-cn"] = "高延迟初始值",
+    ru = "Начало диапазона высокого пинга",
   },
   [SettingNames.PingLabels.None] = {
     en = "- none -",
     ["zh-cn"] = "无",
+    ru = "- нет -",
   },
   [SettingNames.PingLabels.Ping] = {
     en = "Ping",
     ["zh-cn"] = "Ping",
+    ru = "Пинг",
   },
   [SettingNames.PingLabels.Latency] = {
     en = "Latency",
     ["zh-cn"] = "Latency",
+    ru = "Задержка",
   },
   [SettingNames.PingLabels.MS] = {
     en = "ms",
     ["zh-cn"] = "ms",
+    ru = "мс",
   },
   [SettingNames.LabelStyleGroup] = {
     en = "Label Style Settings",
     ["zh-cn"] = "设置文字样式",
+    ru = "Настройки стиля названия",
   },
   [SettingNames.LabelFontSize] = {
     en = "Text Size",
     ["zh-cn"] = "文字大小",
+    ru = "Размер текста",
   },
   [SettingNames.LabelDefaultColor] = {
     en = "Color",
     ["zh-cn"] = "颜色",
+    ru = "Цвет",
   },
   [SettingNames.LabelSidePosition] = {
     en = "Position relative to Ping",
     ["zh-cn"] = "位置",
+    ru = "Позиция относительно пинга",
   },
   [SettingNames.LabelOffsetToPing] = {
     en = "Gap size between Ping",
     ["zh-cn"] = "文字间距",
+    ru = "Расстояние от пинга",
   },
   [SettingNames.LabelYOffset] = {
     en = "Y Offset",
     ["zh-cn"] = "Y轴偏移值",
+    ru = "Смещение по вертикали",
   },
   [SettingNames.LabelUsePingColor] = {
     en = "Match Ping's Color",
     ["zh-cn"] = "参照延迟颜色",
+    ru = "Соответствие цвету пинга",
   },
   [SettingNames.SymbolGroup] = {
     en = "Symbol Style Settings",
     ["zh-cn"] = "设置符号样式",
+    ru = "Настройки стиля символа",
   },
   [SettingNames.Symbol] = {
     en = "Symbol",
     ["zh-cn"] = "符号",
+    ru = "Символ",
   },
   [SettingNames.SymbolSize] = {
     en = "Symbol Size",
     ["zh-cn"] = "符号大小",
+    ru = "Размер символа",
   },
   [SettingNames.SymbolSidePosition] = {
     en = "Position relative to Ping",
     ["zh-cn"] = "符号位置",
+    ru = "Позиция относительно пинга",
   },
   [SettingNames.SymbolColor] = {
     en = "Color",
     ["zh-cn"] = "符号颜色",
+    ru = "Цвет",
   },
   [SettingNames.SymbolOffsetToPing] = {
     en = "Gap size between Ping",
     ["zh-cn"] = "间距",
+    ru = "Расстояние от пинга",
   },
   [SettingNames.SymbolYOffset] = {
     en = "Y Offset",
     ["zh-cn"] = "Y轴偏移值",
+    ru = "Смещение по вертикали",
   },
   [SettingNames.SymbolUsePingColor] = {
     en = "Match Ping's Color",
     ["zh-cn"] = "参照延迟颜色",
+    ru = "Соответствие цвету пинга",
   },
   [SettingNames.SymbolType.Circle] = {
     en = "Circle",
     ["zh-cn"] = "点状",
+    ru = "Круг",
   },
   center = {
     en = "Center",
